7e7ae3efe07f6a34297c8dacfa3294603b28faea
Damien Sarramalho <d.sarramalho@gmail.com>
2021-03-08 18:33:25 +0100

    outside-test

---
+++ animated_bathtub_2_32x32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="animated_bathtub_2_32x32" tilewidth="32" tileheight="32" tilecount="24" columns="8">
+ <image source="../tiled/Modern_Interiors/3_Animated_objects/32x32/spritesheets/animated_bathtub_2_32x32.png" width="256" height="96"/>
+</tileset>
---
+++ animated_door_5_32x32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="animated_door_5_32x32" tilewidth="32" tileheight="32" tilecount="10" columns="5">
+ <image source="../tiled/Modern_Interiors/3_Animated_objects/32x32/spritesheets/animated_door_5_32x32.png" width="160" height="64"/>
+</tileset>
